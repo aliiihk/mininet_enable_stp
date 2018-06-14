0aa5404cdb0414d9ee4668dfad554019ed7cee12
aliiihk <36608098+aliiihk@users.noreply.github.com>
2018-06-13 23:00:25 -0400

    Update enableSTP.py

--- enableSTP.py
+++ enableSTP.py
@@ -4,9 +4,7 @@ import string
 fileName = raw_input("Please enter the name of the file: ")
 STPfileName = "STP_" + fileName
 
-#print("New file name is: " + STPfileName)
 lineBylineCopy = []
-#newFile = open(STPfileName, 'w+')
 switchesInFile = []
 writeSTP = False
 secondLine = False
@@ -16,22 +14,17 @@ if (os.path.exists(fileName) == True):
 	newFile = open(STPfileName, 'w+')
 	with open(fileName) as f:
 		for line in f:
-		#print(line)
 			if "net.get" in line:
 				temp = line[13:16]
 				temp = string.replace(temp, "'", "")
-				#print temp
 				switchesInFile.append(temp)
 	with open(fileName) as f:
 		lastValue = switchesInFile[-1]
 		stringToFind = ("net.get('" + lastValue) 
-		#print("String to find is: " + stringToFind)
 		for line in f:
 			if (secondLine == True and a != -1):
 				newFile.write("import os\nos.system(")
 				newFile.write("\"gnome-terminal -e 'bash -c \\\"sudo ~/pox/pox.py forwarding.l2_pairs info.packet_dump samples.pretty_log log.level --DEBUG   ; exec bash\\\"'\")")
-				#https://stackoverflow.com/questions/31533264/python-syntax-to-open-gnome-terminal-and-execute-multiple-commands
-				#http://www.brianlinkletter.com/using-the-pox-sdn-controller/
 				a = -1
 			newFile.write(line)
 			if stringToFind in line:
